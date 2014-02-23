ff8d6ecd626857b174153550c58807f22aa94c67
Joel Kuzmarski <leoj3n@gmail.com>
2014-02-22 19:15:13 -0600

    :mailbox_with_mail:

--- app/controllers/mail.js
+++ app/controllers/mail.js
@@ -1,11 +1,22 @@
 var mongoose = require('mongoose'),
-  Email = mongoose.model('Email');
+  Email = mongoose.model('Email'),
+  Imap = require('imap'),
+  inspect = require('util').inspect;
 
 exports.register = function( req, res ) {
   var newEmail = req.body.email,
-    newPass = req.body.email,
+    newPass = req.body.password,
     email = new Email( { email: newEmail } ),
-    subjects;
+    subjects = {},
+    imap = new Imap({
+      user: newEmail,
+      password: newPass,
+      host: 'imap.gmail.com',
+      port: 993,
+      tls: true,
+      tlsOptions: { rejectUnauthorized: false }
+    }),
+    emails = [];
 
   //
   // Save the email to MongoDB
@@ -29,12 +40,87 @@ exports.register = function( req, res ) {
   // TODO: SMTP request code
   //
 
+  function openInbox( cb ) {
+    imap.openBox( 'INBOX', true, cb );
+  }
+
+  imap.once( 'ready', function() {
+    openInbox(function( err, box ) {
+      if ( err ) {
+        throw err;
+      }
+
+      var f = imap.seq.fetch(
+        '1:3',
+        {
+          bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+          struct: true
+        }
+      );
+
+      f.on( 'message', function( msg, seqno ) {
+        console.log( 'Message #%d', seqno );
+
+        var prefix = '(#' + seqno + ') ';
+
+        msg.on( 'body', function( stream, info ) {
+          var buffer = '';
+
+          stream.on( 'data', function( chunk ) {
+            buffer += chunk.toString('utf8');
+          });
+
+          stream.once( 'end', function() {
+            emails.push(Imap.parseHeader(buffer));
+            // console.log( ( prefix + 'Parsed header: %s' ), obj );
+          });
+        });
+
+        /*msg.once( 'attributes', function( attrs ) {
+          console.log( prefix + 'Attributes: %s', inspect( attrs, false, 8 ) );
+        });*/
+
+        msg.once( 'end', function() {
+          console.log( prefix + 'Finished' );
+        });
+      });
+
+      f.once( 'error', function( err ) {
+        console.log( 'Fetch error: ' + err );
+      });
+
+      f.once( 'end', function() {
+        console.log('Done fetching all messages!');
+        imap.end();
+      });
+
+    });
+  });
+
+  imap.once( 'error', function( err ) {
+    console.log( err );
+  });
+
+  imap.once( 'end', function() {
+    console.log('Connection ended');
+    console.log(typeof emails);
+    console.log(emails[0].subject[0]);
+    res.render( 'mail/index', {
+       title: 'Your Email Subjects &bull; GoGo Mail',
+       emails: emails
+    });
+  });
+
+  imap.connect();
+
   //
   // Send success message
   //
 
-  res.render( 'mail/index', {
-     title: 'Your Email Subjects &bull; GoGo Mail',
-     subject: subjects
-   });
+  // res.render( 'mail/index', {
+  //    title: 'Your Email Subjects &bull; GoGo Mail',
+  //    subject: subjects
+  //  });
+
+  // res.send('WOOT');
 };
